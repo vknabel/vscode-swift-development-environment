1fe22edf9e117b76608fdfd3ad28642503b60b04
Valentin Knabel <dev@vknabel.com>
2021-03-04 23:17:06 +0100

    Cleanup comments and unused code

--- src/clientMain.ts
+++ src/clientMain.ts
@@ -1,19 +1,13 @@
 "use strict";
 
-import * as fs from "fs";
-import * as path from "path";
 import { commands, DiagnosticCollection, ExtensionContext, Uri, window, workspace } from "vscode";
 import { absolutePath } from "./helpers/AbsolutePath";
 import * as tools from "./toolchain/SwiftTools";
-import * as config from "./vscode/config-helpers";
 import lsp from "./vscode/lsp-interop";
 import output from "./vscode/output-channels";
-import { statusBarItem } from "./vscode/status-bar";
 
 let swiftBinPath: string | null = null;
 let swiftBuildParams: string[] = ["build"];
-let skProtocolProcess: string | null = null;
-let skProtocolProcessAsShellCmd: string | null = null;
 export let diagnosticCollection: DiagnosticCollection;
 
 let mostRecentRunTarget = "";
@@ -28,7 +22,7 @@ export async function activate(context: ExtensionContext) {
   tools.setRunning(false);
   output.build.log("Activating SDE");
 
-  await initConfig();
+  initConfig();
   await lsp.startLSPClient(context);
 
   //commands
@@ -90,46 +84,9 @@ export async function activate(context: ExtensionContext) {
   toolchain.build();
 }
 
-async function initConfig() {
-  checkToolsAvailability();
-}
-
-async function checkToolsAvailability() {
+function initConfig() {
   swiftBinPath = absolutePath(workspace.getConfiguration().get("swift.path.swift_driver_bin"));
   swiftBuildParams = <string[]>workspace.getConfiguration().get("sde.swiftBuildingParams") || [
     "build",
   ];
-  const sourcekitePath = absolutePath(workspace.getConfiguration().get("swift.path.sourcekite"));
-  const sourcekitePathEnableShCmd = workspace
-    .getConfiguration()
-    .get<string>("swift.path.sourcekiteDockerMode");
-  const shellPath = absolutePath(workspace.getConfiguration().get("swift.path.shell"));
-  skProtocolProcess = sourcekitePath;
-  skProtocolProcessAsShellCmd = sourcekitePathEnableShCmd;
-
-  // if (!swiftBinPath || !fs.existsSync(swiftBinPath)) {
-  //   window.showErrorMessage(
-  //     'missing dependent `swift` tool, please configure correct "swift.path.swift_driver_bin"'
-  //   );
-  // }
-  // if (!sourcekitePathEnableShCmd) {
-  //   if (!skProtocolProcess || !fs.existsSync(skProtocolProcess)) {
-  //     const action = await window.showErrorMessage(
-  //       `\`sourcekite\` not found at \`${skProtocolProcess}\`.
-  //       Install it and provide the path to \`swift.path.sourcekite\`.`,
-  //       'Retry', 'Help'
-  //     );
-  //     if (action === "Help") {
-  //       await commands.executeCommand(
-  //         "vscode.open",
-  //         Uri.parse("https://github.com/vknabel/vscode-swift-development-environment/tree/2.11.1#using-sourcekite")
-  //       );
-  //     }
-  //   }
-  // }
-  // if (!shellPath || !fs.existsSync(shellPath)) {
-  //   window.showErrorMessage(
-  //     'missing dependent shell tool, please configure correct "swift.path.shell"'
-  //   );
-  // }
 }
